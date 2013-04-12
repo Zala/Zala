2d5308852842ec4cf9de9f5cf376b32cf52f99bc
Zala Herga <zala.herga@gmail.com>
2013-04-12 17:02:17 +0200

    Changed getModelByBrand to get list by query. Removed getModelByBrandAllNameString. Increased column length in table prijave for type of car and car model. Added line for type of car in assertingPresenter.

--- Cyc/amzsPrijave/src/main/java/si/ijs/mobis/presenters/AmzsIssue.java
+++ Cyc/amzsPrijave/src/main/java/si/ijs/mobis/presenters/AmzsIssue.java
@@ -179,6 +179,14 @@ public class AmzsIssue {
                     return reg;
          }
         
+        public String printBrand() {
+                    String b = inputBrand;
+                    if (inputBrand == null){
+                        b = " / ";
+                    }
+                    return b;
+        }
+          
         public String printModel() {
                     String m = model;
                     if (model == null){
@@ -292,7 +300,7 @@ public class AmzsIssue {
         
         public String importIntoCycModel() throws JSONException, UnknownHostException, CycApiException, IOException {
                     CycObject Mt = _c.getConstantByName("BaseKB");
-                    mapMod = cycService.getModelByBrandAllNameStrings(_c, inputBrand, mapBr);
+                    mapMod = cycService.getModelByBrand(_c, inputBrand, mapBr);
                     String modelConst = String.valueOf(mapMod.get(model));
                     
                     if (!"null".equals(modelConst)){
